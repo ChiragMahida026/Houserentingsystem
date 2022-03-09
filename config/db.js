19803dc43fba8d1d392bc769aae592b0fb6a111d
Mahida Chirag Jitendrasinh <90020661+ChiragMahida026@users.noreply.github.com>
2022-03-09 19:14:14 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-     // @ts-ignore
-     await mongoose.connect(db, { useNewUrlParser: true });
+    // @ts-ignore
+    await mongoose.connect(db, { useNewUrlParser: true });
 
-     console.log("MongoDb Connected.....");
-   } catch (error) {
-     console.error("MOngoDb NotConnected....");
-     //Exit process with failure
+    console.log('MongoDb Connected.....');
+  } catch (error) {
+    console.error('MOngoDb NotConnected....');
+    //Exit process with failure
   }
 };
 module.exports = connectDB;
