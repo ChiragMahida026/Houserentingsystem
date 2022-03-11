608ebccb98db8e8d10f8cc0192d472231a32b2c8
Mahida Chirag Jitendrasinh <90020661+ChiragMahida026@users.noreply.github.com>
2022-03-11 20:56:47 +0530

    Change #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-
+import * as React from 'react';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
-   return (
-     <div>
-       <h1>This is heading1 text</h1>
-       <p>This is sfsfs text</p>
-     </div>
-    
-   );
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Landing/>
+    </BrowserRouter>
+  );
 };
 
 export default App;
