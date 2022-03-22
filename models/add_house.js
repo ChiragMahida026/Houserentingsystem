7f8b3206c6d54d04f60b4ae68559174c8012872b
Dhvani1801 <18bmiit020@gmail.com>
2022-03-22 14:31:23 +0530

    api create

---
+++ models/add_house.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const addhouseSchema = new mongoose.Schema({
+    housepaper: {
+        type: String,
+        required: true, 
+    },
+
+    address: {
+        trim: true,
+        type: String,
+        require: true, 
+    },
+    description: {
+        type: String,
+        required: true, 
+    },
+
+    price: {
+        type:String,
+        required:true,
+    },
+    Image1:{
+        type:String,
+    },
+    Image2:{
+        type:String,
+    },
+    Image3:{
+        type:String,
+    },
+    Image4:{
+        type:String,
+    },
+
+});
+
+//save in variable
+var addhouse = mongoose.model("add_house", addhouseSchema);
+module.exports = addhouse;
