825aab2e6a761f5578ecc3cab930ecda80214c59
Dhvani1801 <18bmiit020@gmail.com>
2022-04-08 11:45:27 +0530

    used sidebar in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import ForgetPassword from "./components/layout/ForgetPassword";
 import Dash_cust from "./components/layout/Dash_cust";
 import Dash_Admin from "./components/layout/Dash_Admin";
 import Dash_Landlord from "./components/layout/Dash_Landlord";
+import Sidebars from "./components/layout/Sidebars";
 
 // let data = localStorage.getItem("myData");
 // console.log(data);
@@ -24,40 +25,43 @@ import Dash_Landlord from "./components/layout/Dash_Landlord";
 // }
 const App = () => {
   return (
-    <>
-      {/* {localStorage.getItem("myData") === "C" ? <Navbar /> : "unauthorized"} */}
-      {/* <Navbar /> */}
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
+    //  <>
+    //   {/* {localStorage.getItem("myData") === "C" ? <Navbar /> : "unauthorized"} */}
+    //   {/* <Navbar /> */}
+    //   <Fragment>
+    //     <Navbar />
+    //     <Route exact path="/" component={Landing} />
 
-        <section className="container">
-          <Switch>
-            <Route
-              exact
-              path="/customer_reg"
-              // @ts-ignore
-              component={CustomerRegistration}
-            />
-            <Route exact path="/login" component={Login} />
-            <Route
-              exact
-              path="/landlord_reg"
-              component={LandlordRegistration}
-            />
-            <Route exact path="/error" component={Error_page} />
-            <Route exact path="/forgetpass" component={ForgetPassword} />
-            {/* //Dashboard */}
-            {localStorage.getItem("myData") === "C"} ?{" "}
-            <Route exact path="/dashadmin" component={Dash_Admin} /> :""
-            {localStorage.getItem("myData") === "L"} ?{" "}
-            <Route exact path="/dashadmin" component={Dash_Admin} /> :""
-            {localStorage.getItem("myData") === "A"} ?{" "}
-            <Route exact path="/dashlandlord" component={Dash_Landlord} /> :""
-          </Switch>
-        </section>
-      </Fragment>
-      {localStorage.getItem("myData") === "C" ? "" : <Footer />}
+    //     <section className="container">
+    //       <Switch>
+    //         <Route
+    //           exact
+    //           path="/customer_reg"
+    //           // @ts-ignore
+    //           component={CustomerRegistration}
+    //         />
+    //         <Route exact path="/login" component={Login} />
+    //         <Route
+    //           exact
+    //           path="/landlord_reg"
+    //           component={LandlordRegistration}
+    //         />
+    //         <Route exact path="/error" component={Error_page} />
+    //         <Route exact path="/forgetpass" component={ForgetPassword} />
+    //         {/* //Dashboard */}
+    //         {localStorage.getItem("myData") === "C"} ?{" "}
+    //         <Route exact path="/dashadmin" component={Dash_Admin} /> :""
+    //         {localStorage.getItem("myData") === "L"} ?{" "}
+    //         <Route exact path="/dashadmin" component={Dash_Admin} /> :""
+    //         {localStorage.getItem("myData") === "A"} ?{" "}
+    //         <Route exact path="/dashlandlord" component={Dash_Landlord} /> :""
+    //       </Switch>
+    //     </section>
+    //   </Fragment>
+    //   {localStorage.getItem("myData") === "C" ? "" : <Footer />}
+    // </>
+    <>
+      <Sidebars></Sidebars>
     </>
   );
 };
