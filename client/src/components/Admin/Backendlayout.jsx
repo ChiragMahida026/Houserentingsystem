577641e2f2901fb1ac7c20ab9a4212c80e56a525
Dhvani1801 <18bmiit020@gmail.com>
2022-04-12 01:41:40 +0530

    Admin Dashboard

---
+++ client/src/components/Admin/Backendlayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Cards from "./card";
+function Backendlayout(props) {
+  return (
+    <div>
+      Backendlayout
+      <Cards />
+      <Outlet />
+    </div>
+  );
+}
+
+export default Backendlayout;
