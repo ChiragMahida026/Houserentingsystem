108f09f5869badc7cf232e5f2100777a641155d6
Dhvani1801 <18bmiit020@gmail.com>
2022-05-06 21:52:10 +0530

    aboutus page

--- client/src/components/layout/About.jsx
+++ client/src/components/layout/About.jsx
@@ -5,23 +5,24 @@ const About = () => {
     <>
       <div
         className="kjdkadhakldhakldh"
-        style={{ backgroundColor: "cornsilk", marginTop: "5%" }}
+        style={{ backgroundColor: "#fff", marginTop: "5%" }}
       >
         <h1>House Renting System</h1>
-        <p
-          className="hdkahdkajdhajk"
-          style={{ marginTop: "1%", marginBottom: "5%" }}
-        >
-              House Renting System has made creating a listing a simple process,and
+        <p 
+          style={{ marginTop: "1%", marginBottom: "5%",fontSize:"25px",fontStyle:"italic",textAlign:"center",color:"yellowgreen",fontWeight:"bold"}}
+        > 
+
+          <p className="mt-2">  House Renting System has made creating a listing a simple process,and
           property details and images, set your monthly rent price, and then
           publish the listing. Landlords can update the profile online whenever
-          they want. 
+          they want.</p>
           
-              House Renting System offers 1BHK,2BHK and 3BHK flats for
-          rent It is an online forum where tenants can exchange information
+          <p className="mt-2">   House Renting System offers 1BHK, 2BHK and 3BHK flats for
+          rent It is an online forum where Customer can exchange information
           about real estate properties quickly, effectively and inexpensively.
-          We make online property search easier, quicker and smarter.
-        </p>
+          We make online property search easier, quicker and smarter.</p>
+
+        </p> 
       </div>
     </>
   );
